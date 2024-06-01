240bf251e2d223af4af37293fdd152649c8b03b6
acekavi <avishkakavinda@proton.me>
2024-06-01 15:20:01 +0530

    chore: Update .gitignore and package.json

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -32,10 +32,11 @@ app.get('/', (req, res) => {
 
 app.post('/users', async (req, res) => {
     const { name, email } = req.body;
+
     try {
         const user = await User.create({ name, email });
         res.status(201).json(user);
-    } catch (error) {
+    } catch (error: any) {
         res.status(400).json({ error: error.message });
     }
 });
@@ -44,7 +45,7 @@ app.get('/users', async (req, res) => {
     try {
         const users = await User.findAll();
         res.status(200).json(users);
-    } catch (error) {
+    } catch (error: any) {
         res.status(400).json({ error: error.message });
     }
 });
