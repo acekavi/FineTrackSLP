a4067a2751a6481d173b2d7b4ba074dc8c068b23
Rashmika Samarakoon <thilinirashmika098@gmail.com>
2024-06-05 11:28:31 +0530

    violations pedestrian-type driver-type

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PedestrianTypeComponent } from './violations/pedestrian-type/pedestrian-type.component';
+import { DriverTypeComponent } from './violations/driver-type/driver-type.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'pedestrian', component: PedestrianTypeComponent },
+    { path: 'driver', component: DriverTypeComponent },
 ];
---
+++ Frontend/src/app/violations/driver-type/driver-type.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatUiModule } from '../../modules/matui.module';
+
+@Component({
+  selector: 'app-driver-type',
+  standalone: true,
+  imports: [
+    MatUiModule,
+  ],
+  templateUrl: './driver-type.component.html',
+  styleUrl: './driver-type.component.scss'
+})
+export class DriverTypeComponent {
+
+}
